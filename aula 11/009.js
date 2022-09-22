9e15c55b5eb9f532a813c6f2b498c29246177d40
PedroParavidino <pedrinho.paravidino@gmail.com>
2022-09-22 17:04:04 -0300

    j

---
+++ aula 11/009.js
@@ -0,0 +1,8 @@
+var pais = 'frança'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'brasil') {
+    console.log('Voce e Estrangeiro')
+}
+else{
+    console.log('Voce e Brasileiro')
+}
---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 66 
+console.log(`a velocidade do seu carro e ${vel}km/h`) 
+if (vel> 60) { //condição simples
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log('Dirija sempre com cinto de segurança')
